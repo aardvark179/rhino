4931971ce0bace0bd8b86bd0677b4459eea536fd
Greg Brail <gbrail@users.noreply.github.com>
2025-06-13 18:00:24 -0500

    Replace internal use of old "FunctionAndThis" methods
    Replace usages of all the old "getXFunctionAndThis" functions with the
    newer functions that return a LookupResult. This keeps us consistent
    with the rest of the runtime. All of the old functions are deprecated and
    could be removed if we can be sure that they're no longer used outside
    this project.

--- rhino/src/main/java/org/mozilla/javascript/NativeArray.java
+++ rhino/src/main/java/org/mozilla/javascript/NativeArray.java
@@ -1249,13 +1249,9 @@ public class NativeArray extends IdScriptableObject implements List {
 
                     } else {
                         if (toLocale) {
-                            Callable fun;
-                            Scriptable funThis;
-                            fun =
-                                    ScriptRuntime.getPropFunctionAndThis(
-                                            elem, "toLocaleString", cx, scope);
-                            funThis = ScriptRuntime.lastStoredScriptable(cx);
-                            elem = fun.call(cx, scope, funThis, ScriptRuntime.emptyArgs);
+                            var fun =
+                                    ScriptRuntime.getPropAndThis(elem, "toLocaleString", cx, scope);
+                            elem = fun.call(cx, scope, ScriptRuntime.emptyArgs);
                         }
                         result.append(ScriptRuntime.toString(elem));
                     }
