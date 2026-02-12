39b18e971d5f904cfa096cb2ff5763b46e0c5da9
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-02-12 17:08:11 +0000

    Implement runtime semantics of new target.

--- rhino/src/main/java/org/mozilla/javascript/BaseFunction.java
+++ rhino/src/main/java/org/mozilla/javascript/BaseFunction.java
@@ -133,17 +133,14 @@ public class BaseFunction extends ScriptableObject implements Function {
                 });
     }
 
-    public BaseFunction() {
+    public BaseFunction(VarScope scope) {
+        declarationScope = scope;
         createProperties();
-    }
-
-    public BaseFunction(boolean isGenerator) {
-        createProperties();
-        this.isGeneratorFunction = isGenerator;
     }
 
     public BaseFunction(VarScope scope, Scriptable prototype) {
         super(scope, prototype);
+        declarationScope = scope;
         createProperties();
         ScriptRuntime.setBuiltinProtoAndParent(this, scope, TopLevel.Builtins.Function);
     }
@@ -479,12 +476,12 @@ public class BaseFunction extends ScriptableObject implements Function {
 
     private static Scriptable js_constructor(
             Context cx, JSFunction f, Object nt, VarScope s, Object thisObj, Object[] args) {
-        return jsConstructor(cx, f.getDeclarationScope(), args, false);
+        return jsConstructor(cx, f, nt, f.getDeclarationScope(), args, false);
     }
 
     private static Scriptable js_gen_constructor(
             Context cx, JSFunction f, Object nt, VarScope s, Object thisObj, Object[] args) {
-        return jsConstructor(cx, f.getDeclarationScope(), args, true);
+        return jsConstructor(cx, f, nt, f.getDeclarationScope(), args, true);
     }
 
     private static BaseFunction realFunction(Object thisObj, String functionName) {
@@ -521,12 +518,16 @@ public class BaseFunction extends ScriptableObject implements Function {
 
     /** Should be overridden. */
     @Override
-    public Object call(Context cx, VarScope scope, Object thisObj, Object[] args) {
+    public final Object call(Context cx, VarScope scope, Object thisObj, Object[] args) {
+        return call(cx, Undefined.instance, scope, thisObj, args);
+    }
+
+    public Object call(Context cx, Object nt, VarScope s, Object thisObj, Object[] args) {
         return Undefined.instance;
     }
 
     @Override
-    public Scriptable construct(Context cx, VarScope scope, Object[] args) {
+    public Scriptable construct(Context cx, Object nt, VarScope scope, Object[] args) {
         if (cx.getLanguageVersion() >= Context.VERSION_ES6 && this.getHomeObject() != null) {
             // Only methods have home objects associated with them
             throw ScriptRuntime.typeErrorById("msg.not.ctor", getFunctionName());
@@ -534,7 +535,7 @@ public class BaseFunction extends ScriptableObject implements Function {
 
         Scriptable result = createObject(cx, scope);
         if (result == null) {
-            Object val = call(cx, scope, null, args);
+            Object val = call(cx, nt, scope, null, args);
             if (!(val instanceof Scriptable)) {
                 // It is program error not to return Scriptable from
                 // the call method if createObject returns null.
@@ -556,7 +557,7 @@ public class BaseFunction extends ScriptableObject implements Function {
                 result.setParentScope(parent);
             }
         } else {
-            Object val = call(cx, scope, result, args);
+            Object val = call(cx, nt, scope, result, args);
             if (val instanceof Scriptable) {
                 result = (Scriptable) val;
             }
@@ -574,6 +575,25 @@ public class BaseFunction extends ScriptableObject implements Function {
     public Scriptable createObject(Context cx, VarScope scope) {
         Scriptable newInstance = new NativeObject();
         newInstance.setPrototype(getClassPrototype());
+        newInstance.setParentScope(getParentScope());
+        return newInstance;
+    }
+
+    public Scriptable createObject(Context cx, VarScope scope, Object nt) {
+        Scriptable newInstance = new NativeObject();
+        Object proto;
+        if (nt instanceof JSFunction) {
+            proto = ((JSFunction) nt).getPrototypeProperty();
+        } else {
+            proto = Undefined.instance;
+        }
+
+        if (proto instanceof Scriptable) {
+            newInstance.setPrototype((Scriptable) proto);
+        } else {
+            newInstance.setPrototype(getClassPrototype());
+        }
+
         newInstance.setParentScope(getParentScope());
         return newInstance;
     }
@@ -732,7 +752,12 @@ public class BaseFunction extends ScriptableObject implements Function {
     void setArguments(Object caller) {}
 
     private static Scriptable jsConstructor(
-            Context cx, VarScope scope, Object[] args, boolean isGeneratorFunction) {
+            Context cx,
+            JSFunction f,
+            Object nt,
+            VarScope scope,
+            Object[] args,
+            boolean isGeneratorFunction) {
         int arglen = args.length;
         StringBuilder sourceBuf = new StringBuilder();
 
@@ -767,7 +792,7 @@ public class BaseFunction extends ScriptableObject implements Function {
 
         String sourceURI = ScriptRuntime.makeUrlForGeneratedScript(false, filename, linep[0]);
 
-        TopLevel global = ScriptableObject.getTopLevelScope(scope);
+        TopLevel global = ScriptableObject.getTopLevelScope(f.getDeclarationScope());
 
         ErrorReporter reporter;
         reporter = DefaultErrorReporter.forEval(cx.getErrorReporter());
@@ -779,7 +804,18 @@ public class BaseFunction extends ScriptableObject implements Function {
 
         // Compile with explicit interpreter instance to force interpreter
         // mode.
-        return cx.compileFunction(global, source, evaluator, reporter, sourceURI, 1, null);
+        var res =
+                (JSFunction)
+                        cx.compileFunction(global, source, evaluator, reporter, sourceURI, 1, null);
+
+        if (res.getDescriptor().isES6Generator()) {
+            ScriptRuntime.setBuiltinProtoAndParent(
+                    (ScriptableObject) res, f, nt, scope, TopLevel.Builtins.GeneratorFunction);
+        } else {
+            ScriptRuntime.setBuiltinProtoAndParent(
+                    (ScriptableObject) res, f, nt, scope, TopLevel.Builtins.Function);
+        }
+        return res;
     }
 
     public void setHomeObject(Scriptable homeObject) {
@@ -796,15 +832,12 @@ public class BaseFunction extends ScriptableObject implements Function {
                 && this.getHomeObject() != null);
     }
 
-    private static final int Id_constructor = 1,
-            Id_toString = 2,
-            Id_toSource = 3,
-            Id_apply = 4,
-            Id_call = 5,
-            Id_bind = 6,
-            SymbolId_hasInstance = 7,
-            MAX_PROTOTYPE_ID = SymbolId_hasInstance;
+    @Override
+    public VarScope getDeclarationScope() {
+        return declarationScope;
+    }
 
+    private final VarScope declarationScope;
     private Object prototypeProperty;
     private Object argumentsObj = NOT_FOUND;
     private Object nameValue = null;
