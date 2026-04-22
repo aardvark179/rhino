fea0d55eae577e1bd31b09e0390062a912547039
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-04-22 11:14:27 +0100

    Fix finally handling in subsidiary methods.

--- rhino/src/main/java/org/mozilla/classfile/ClassFileWriter.java
+++ rhino/src/main/java/org/mozilla/classfile/ClassFileWriter.java
@@ -1247,6 +1247,12 @@ public class ClassFileWriter {
 
     public int acquireLabel() {
         int top = itsLabelTableTop;
+        if (DEBUGLABELS) {
+            if (DEBUGCODEORIGINS) {
+                printOrigin();
+            }
+            System.err.printf("Acquired label %x.\n", top);
+        }
         if (itsLabelTable == null || top == itsLabelTable.length) {
             if (itsLabelTable == null) {
                 itsLabelTable = new int[MIN_LABEL_TABLE_SIZE];
@@ -1262,6 +1268,12 @@ public class ClassFileWriter {
     }
 
     public void markLabel(int label) {
+        if (DEBUGLABELS) {
+            if (DEBUGCODEORIGINS) {
+                printOrigin();
+            }
+            System.err.printf("Mark label %x.\n", label);
+        }
         if (!(label < 0)) throw new IllegalArgumentException("Bad label, no biscuit");
 
         label &= 0x7FFFFFFF;
@@ -1318,7 +1330,7 @@ public class ClassFileWriter {
             int pc = itsLabelTable[label];
             if (pc == -1) {
                 // Unlocated label
-                throw new RuntimeException("unlocated label");
+                throw new RuntimeException(String.format("unlocated label %x", label));
             }
             // -1 to get delta from instruction start
             addSuperBlockStart(pc);
