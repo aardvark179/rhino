eb3e35638dcc73f7bb992fc6b7254dd6a6b3d52a
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-06-18 12:14:54 +0100

    Enable async-iterator 262 tests.

--- tests/src/test/java/org/mozilla/javascript/tests/Test262SuiteTest.java
+++ tests/src/test/java/org/mozilla/javascript/tests/Test262SuiteTest.java
@@ -111,7 +111,6 @@ public class Test262SuiteTest {
                     Arrays.asList(
                             "Atomics",
                             "IsHTMLDDA",
-                            "async-iteration",
                             "class",
                             "class-fields-private",
                             "class-fields-public",
