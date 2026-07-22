52eae211bd6ccffc98369df7eb526a18b791e8e4
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-07-22 15:52:34 +0100

    Start working towards wider descriptor usage.

--- rhino/src/main/java/org/mozilla/javascript/CompactSlot.java
+++ rhino/src/main/java/org/mozilla/javascript/CompactSlot.java
@@ -1,24 +1,55 @@
 package org.mozilla.javascript;
 
-public abstract class CompactSlot<T extends PropHolder<T>, O extends ScriptableObject>
-        extends Slot<T> {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.Objects;
+
+public abstract class CompactSlot<T extends CompactSlot.Descriptor<T, U, O>, U extends PropHolder<U>, O extends ScriptableObject>
+        extends Slot<U> {
 
     public abstract static class Descriptor<
-            T extends CompactSlot<U, O>, U extends PropHolder<U>, O extends ScriptableObject> {
+            T extends CompactSlot.Descriptor<T, U, O>, U extends PropHolder<U>, O extends ScriptableObject>
+implements Serializable {
+        private final Object name;
+        private int indexOrHash;
 
-        public abstract T createSlot(O owner, int attr);
+        protected Descriptor(Object name, int indexOrHash) {
+            this.name = name;
+            this.indexOrHash = name == null ? indexOrHash : name.hashCode();
+        }
+
+        public Object getName() {
+            return name;
+        }
+
+        public int getIndexOrHash() {
+            return indexOrHash;
+        }
+
+        public abstract CompactSlot<T, U, O> createSlot(O owner, int attr);
+
+        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+            in.defaultReadObject();
+            if (name != null) {
+                indexOrHash = name.hashCode();
+            }
+        }
     }
 
     private short attributes;
+    protected final T descriptor;
 
-    CompactSlot(int attr) {
+    CompactSlot(T descriptor, int attr) {
         super();
         this.attributes = (short) attr;
+        this.descriptor = descriptor;
     }
 
-    CompactSlot(CompactSlot<T, O> oldSlot) {
+    CompactSlot(CompactSlot<T, U, O> oldSlot) {
         super();
         this.attributes = oldSlot.attributes;
+        this.descriptor = oldSlot.descriptor;
     }
 
     @Override
@@ -29,5 +60,26 @@ public abstract class CompactSlot<T extends PropHolder<T>, O extends ScriptableO
     @Override
     void setAttributes(int value) {
         this.attributes = (short) value;
+    }
+
+    @Override
+    public final boolean keyMatches(Object key, int indexOrHash) {
+        return indexOrHash == this.descriptor.getIndexOrHash()
+                && Objects.equals(this.descriptor.getName(), key);
+    }
+
+    @Override
+    public final Object getKey() {
+        return descriptor.getName() != null ? descriptor.getName() : descriptor.getIndexOrHash();
+    }
+
+    @Override
+    public final Object getName() {
+        return descriptor.getName();
+    }
+
+    @Override
+    public final int getIndexOrHash() {
+        return descriptor.getIndexOrHash();
     }
 }
