f568c57d1281b1920616fab6014467081b106b70
duncan.macgregor <duncan.macgregor@servicenow.com>
2026-07-23 10:17:30 +0100

    Start moving logic from slots to descriptors.

--- rhino/src/main/java/org/mozilla/javascript/CompactSlot.java
+++ rhino/src/main/java/org/mozilla/javascript/CompactSlot.java
@@ -5,12 +5,17 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Objects;
 
-public abstract class CompactSlot<T extends CompactSlot.Descriptor<T, U, O>, U extends PropHolder<U>, O extends ScriptableObject>
+public abstract class CompactSlot<
+                T extends CompactSlot.Descriptor<T, U, O>,
+                U extends PropHolder<U>,
+                O extends ScriptableObject>
         extends Slot<U> {
 
     public abstract static class Descriptor<
-            T extends CompactSlot.Descriptor<T, U, O>, U extends PropHolder<U>, O extends ScriptableObject>
-implements Serializable {
+                    T extends CompactSlot.Descriptor<T, U, O>,
+                    U extends PropHolder<U>,
+                    O extends ScriptableObject>
+            implements Serializable {
         private final Object name;
         private int indexOrHash;
 
@@ -28,6 +33,11 @@ implements Serializable {
         }
 
         public abstract CompactSlot<T, U, O> createSlot(O owner, int attr);
+
+        public abstract Object getValue(CompactSlot<T, U, O> slot, U start);
+
+        public abstract boolean setValue(
+                CompactSlot<T, U, O> slot, Object value, O owner, U start, boolean isThrow);
 
         private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
             in.defaultReadObject();
@@ -48,6 +58,20 @@ implements Serializable {
         super(oldSlot);
         this.descriptor = oldSlot.descriptor;
     }
+
+    @Override
+    public final Object getValue(U start) {
+        return descriptor.getValue(this, start);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public final boolean setValue(Object value, U owner, U start, boolean isThrow) {
+        return descriptor.setValue(this, value, (O) owner, start, isThrow);
+    }
+
+    @Override
+    public final void setValueFromDescriptor(Object value, U owner, U start, boolean isThrow) {}
 
     @Override
     public final boolean keyMatches(Object key, int indexOrHash) {
